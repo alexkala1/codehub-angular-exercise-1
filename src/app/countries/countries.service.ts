51f753aabdafb35ef29232142dad8685cb8309fb
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-05 19:32:26 +0300

    Add service functionality

--- src/app/countries/countries.service.ts
+++ src/app/countries/countries.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Countries } from './countries';
+
+const url = 'https://restcountries.eu/rest/v2/';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class CountriesService {
 
-  constructor() { }
+	constructor(private http: HttpClient) { }
+
+	getCountries(): Observable<Countries[]> {
+		return this.http.get<Countries[]>(url);
+	}
 }
