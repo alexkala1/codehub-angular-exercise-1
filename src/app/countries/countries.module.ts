24f96adc9a2c0e2766ec3b4f76ebe7e5778ed963
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-05 19:12:49 +0300

    Initial Commit

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountriesListComponent } from './countries-list/countries-list.component';
+
+
+
+@NgModule({
+  declarations: [CountriesListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountriesModule { }
