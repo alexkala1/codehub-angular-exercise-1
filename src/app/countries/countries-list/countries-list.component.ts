7be554664f9708c7bac0d957342654a14ad418e6
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-05 19:31:58 +0300

    Add component functionality

--- src/app/countries/countries-list/countries-list.component.ts
+++ src/app/countries/countries-list/countries-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Countries } from '../countries';
+import { CountriesService } from '../countries.service';
 
 @Component({
-  selector: 'app-countries-list',
-  templateUrl: './countries-list.component.html',
-  styleUrls: ['./countries-list.component.scss']
+	selector: 'app-countries-list',
+	templateUrl: './countries-list.component.html',
+	styleUrls: ['./countries-list.component.scss']
 })
 export class CountriesListComponent implements OnInit {
 
-  constructor() { }
+	countries: Countries[];
 
-  ngOnInit(): void {
-  }
+	constructor(private countriesService: CountriesService) { }
 
+	ngOnInit(): void {
+		this.countriesService.getCountries().subscribe(countries => {
+			this.countries = countries;
+		});
+	}
 }
